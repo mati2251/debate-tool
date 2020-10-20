8064058b13d189680df514dbe69be0a0124b3b3f
lunax <mlunaelumen@gmail.com>
2020-10-20 12:00:34 +0200

    feat:timer input is string

--- index.js
+++ index.js
@@ -39,21 +39,27 @@ const buttonHandler = () => {
 	const idea = document.getElementById("idea")
 	const howManyPeopelAgree = document.getElementById("howManyPeopleAgree")
 	const howManyPeopelDisagree = document.getElementById("howManyPeopleDisagree")
-	const time = document.getElementById("time")
-	const blockTime = document.getElementById("blockTime")
-	if (isNaN(howManyPeopelAgree.value) || parseInt(howManyPeopelAgree.value) < 0 || parseInt(howManyPeopelDisagree.value) < 0 || isNaN(time.valueAsNumber) || time.valueAsNumber < blockTime.valueAsNumber) {
+	let time = document.getElementById("time").value
+	timea = time.split(':')
+	time = ((parseInt(timea[0]) || 0) + (parseInt(timea[1]) || 0))
+	let blockTime = document.getElementById("blockTime").value
+	blockTimea = blockTime.split(':')
+	blockTime = ((parseInt(blockTimea[0]) || 0) * 60) + (parseInt(blockTimea[1]) || 0)
+	if (isNaN(howManyPeopelAgree.value) || parseInt(howManyPeopelAgree.value) < 0 || parseInt(howManyPeopelDisagree.value) < 0 ||
+		isNaN(time) || (parseInt(time) < parseInt(blockTime))) {
 		document.getElementById("warring").style.display = "block"
 	} else {
+		console.log(time)
 		document.getElementById("warring").style.display = "none";
 		localStorage.setItem("idea", idea.value)
 		localStorage.setItem("peopleAgree", howManyPeopelAgree.value)
 		localStorage.setItem("peopleDisagree", howManyPeopelDisagree.value)
-		localStorage.setItem("time", (time.valueAsNumber/60).toString())
-		localStorage.setItem("blockTime", (blockTime.valueAsNumber/60).toString())
-		for(let i=1; i<=howManyPeopelAgree.value; i++){
+		localStorage.setItem("time", (time).toString())
+		localStorage.setItem("blockTime", (blockTime).toString())
+		for (let i = 1; i <= howManyPeopelAgree.value; i++) {
 			localStorage.setItem(`name-${i}`, document.getElementById(`name-${i}`).value)
 		}
-		for(let i=1; i<=howManyPeopelDisagree.value; i++){
+		for (let i = 1; i <= howManyPeopelDisagree.value; i++) {
 			localStorage.setItem(`name-dis-${i}`, document.getElementById(`name-dis-${i}`).value)
 		}
 		window.open('file://' + __dirname + '/timer.html');
